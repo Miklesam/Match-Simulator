464de95a4641a82f752ee47eb32902c2b7d9d30c
Miklesami <mikle.samarkin@gmail.com>
2020-06-06 13:30:05 +0300

    stats init

--- app/src/main/java/com/miklesam/dotamatchsimulator/HeroHolder.kt
+++ app/src/main/java/com/miklesam/dotamatchsimulator/HeroHolder.kt
@@ -6,15 +6,25 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.hero_item.view.*
 
-class HeroHolder (itemView:View, var heroListener: OnHeroListener):RecyclerView.ViewHolder(itemView),View.OnClickListener{
+class HeroHolder(itemView: View, var heroListener: OnHeroListener) :
+    RecyclerView.ViewHolder(itemView), View.OnClickListener {
     override fun onClick(p0: View?) {
-        heroListener.onHeroClick((adapterPosition),this)
+        heroListener.onHeroClick((adapterPosition), this)
     }
-    val heroIcon :ImageView
-    val heroName:TextView
-    init{
-        heroIcon=itemView.heroIcon
-        heroName=itemView.heroName
+
+    val heroIcon: ImageView
+    val heroName: TextView
+
+    val heroLaining: TextView
+    val heroFighting: TextView
+    val heroLateGame: TextView
+
+    init {
+        heroIcon = itemView.heroIcon
+        heroName = itemView.heroName
+        heroLaining = itemView.heroLaining
+        heroFighting = itemView.heroFighting
+        heroLateGame = itemView.heroLateGame
 
         itemView.setOnClickListener(this)
     }
--- app/src/main/java/com/miklesam/dotamatchsimulator/HeroesAdapter.kt
+++ app/src/main/java/com/miklesam/dotamatchsimulator/HeroesAdapter.kt
@@ -4,14 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.miklesam.dotamanager.datamodels.Hero
+import kotlinx.android.synthetic.main.hero_item.view.*
 
 class HeroesAdapter(val heroListener: OnHeroListener) : RecyclerView.Adapter<HeroHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroHolder {
         val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.hero_item,parent,false)
-        return HeroHolder(itemView,heroListener)
+            .inflate(R.layout.hero_item, parent, false)
+        return HeroHolder(itemView, heroListener)
     }
 
     override fun getItemCount(): Int {
@@ -20,8 +21,12 @@ class HeroesAdapter(val heroListener: OnHeroListener) : RecyclerView.Adapter<Her
 
     override fun onBindViewHolder(holder: HeroHolder, position: Int) {
         val hero = Heroes.values()[position]
-        holder.heroName.text=hero.heroName
+        holder.heroName.text = hero.heroName
         holder.heroIcon.setImageResource(hero.image_pick)
+
+        holder.heroLaining.text = hero.laining.toString()
+        holder.heroFighting.text = hero.fighting.toString()
+        holder.heroLateGame.text = hero.lateGame.toString()
     }
 
 }
